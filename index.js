2c0c7760d02ba418339bced0646703da3144efb7
Gabriel <gaabicarp@gmail.com>
2019-12-18 14:56:44 -0300

    nueva credencial

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// require("./functions/database")
+require("./functions/database")
 require("dotenv").config();
 
 //Middlewares
